14c46c6e510c2740decc722c849e3d6c1a874cce
Ye99-AungKhant <yeaungkhant077@gmail.com>
2022-12-30 15:26:52 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import axios from "axios"
 const App = () => {
 
   const [name, setName] = useState('')
   const [age, setAge] = useState('')
+  const [getApi, setGetApi] = useState([])
+  const [refresh, setRefresh] = useState([])
 
   const add = (e) =>{
     e.preventDefault()
-    console.log(name, age);
+    axios.post('https://sheet.best/api/sheets/dc654825-9f47-4723-90f9-fc13f6c71204',{
+      name, age
+    }).then(res => setRefresh(res.data))
   }
+
+
+  useEffect(() => {
+    axios.get('https://sheet.best/api/sheets/dc654825-9f47-4723-90f9-fc13f6c71204')
+    .then(res => 
+      setGetApi(res.data)
+    )
+  }, [refresh])
 
   return (
     <div>
@@ -17,6 +29,12 @@ const App = () => {
         <input type="text" className="form-control" onChange={(e) => setAge(e.target.value)}/>
         <button onClick={add}>Add</button>
       </form>
+
+    {getApi.map((data, index) => {
+      return (
+        <p key={index}>{data.name}, {data.age}</p>
+      )
+    })}
     </div>
   )
 }
